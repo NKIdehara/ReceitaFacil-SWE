fb165c640bf6243ce567fb54c2f2957ae6610a4c
Nelson Idehara <idehara@yahoo.com>
2024-01-07 00:23:38 -0600

    R0.13k
    Teste de deployment

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Receitas from "./pages/Receitas";
-import Usuarios from "./pages/Usuarios";
+// import { BrowserRouter } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Receitas from "./pages/Receitas";
+// import Usuarios from "./pages/Usuarios";
+
+
+function sum(a, b) {
+    return a + b;
+}
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
 
 // test('verifica se a tela inicial está renderizada corretamente', () => {
 //     render(<Home />, {wrapper: BrowserRouter})
