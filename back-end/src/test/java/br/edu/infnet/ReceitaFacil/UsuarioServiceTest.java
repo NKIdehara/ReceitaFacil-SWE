404bf83701e6bf4f7d2df9900778aa5a7d79919e
Nelson Idehara <idehara@yahoo.com>
2024-02-24 00:33:07 -0600

    R1.05
    - Testes unitários para Front-End e Back-End

--- back-end/src/test/java/br/edu/infnet/ReceitaFacil/UsuarioServiceTest.java
+++ back-end/src/test/java/br/edu/infnet/ReceitaFacil/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package br.edu.infnet.ReceitaFacil;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
@@ -9,12 +10,14 @@ import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.ExportedUserRecord;
@@ -30,13 +33,20 @@ public class UsuarioServiceTest {
     @Test
     public void ReceitaFacil_TestaConexaoComFirebase_True() throws InterruptedException, ExecutionException, IOException {
         // Arrange
-		ClassLoader classLoader = UsuarioServiceTest.class.getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource("google-services.json")).getFile());
-		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        GoogleCredentials credentials = null;
+        try {
+            ClassLoader classLoader = UsuarioServiceTest.class.getClassLoader();
+            File file = new File(Objects.requireNonNull(classLoader.getResource("google-services.json")).getFile());
+            FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+            credentials = GoogleCredentials.fromStream(serviceAccount);
+        } catch (Exception e) {
+            String GOOGLE_APPLICATION_CREDENTIALS = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+            credentials = ServiceAccountCredentials.fromStream(new ByteArrayInputStream(GOOGLE_APPLICATION_CREDENTIALS.getBytes(StandardCharsets.UTF_8)));   
+        }
         FirebaseOptions options = FirebaseOptions.builder()
-			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-			.build();
-		FirebaseApp.initializeApp(options);		
+            .setCredentials(credentials)
+            .build();
+		FirebaseApp.initializeApp(options);
 
         // Act
         GetUsersResult result = FirebaseAuth.getInstance().getUsersAsync(Arrays.asList()).get();        
@@ -50,11 +60,18 @@ public class UsuarioServiceTest {
         // Arrange
         int num_usuarios = 8;
         List<Usuario> usuarios = new ArrayList<Usuario>();
-		ClassLoader classLoader = UsuarioServiceTest.class.getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource("google-services.json")).getFile());
-		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        GoogleCredentials credentials = null;
+        try {
+            ClassLoader classLoader = UsuarioServiceTest.class.getClassLoader();
+            File file = new File(Objects.requireNonNull(classLoader.getResource("google-services.json")).getFile());
+            FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+            credentials = GoogleCredentials.fromStream(serviceAccount);
+        } catch (Exception e) {
+            String GOOGLE_APPLICATION_CREDENTIALS = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+            credentials = ServiceAccountCredentials.fromStream(new ByteArrayInputStream(GOOGLE_APPLICATION_CREDENTIALS.getBytes(StandardCharsets.UTF_8)));   
+        }
         FirebaseOptions options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setCredentials(credentials)
             .build();
 		FirebaseApp.initializeApp(options);
 
